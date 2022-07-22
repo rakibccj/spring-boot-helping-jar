91d32d2835baeb1da270b9b32c18bd4ccac23d8d
rakibccj <rakibccj@gmail.com>
2022-07-22 15:50:07 +0600

    spring batch project initiation
        -- Added required dependencies (spring batch, web, lombok, jpa, mysql driver)
        -- Added properties for mysql database connection
        -- Added Ignore command for batch job at properties file

---
+++ spring-batch/src/test/java/com/brother/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brother.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
